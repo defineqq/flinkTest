86a621e0d93ee1db4454674b8d9e78baa778a740
defineqq <914385955@qq.com>
2023-04-17 15:07:15 +0800

    更改依赖方式，去掉父级依赖，以后所有依赖都是用自身项目内的依赖包，so 自身项目内依赖的版本找不到，去最外层父级pom中找version信息，最外层pom内的dependency中注释

--- catalog/src/main/java/catalogtest.java
+++ catalog/src/main/java/catalogtest.java
@@ -1,22 +1,14 @@
-import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
-import org.apache.flink.api.common.eventtime.WatermarkStrategy;
-import org.apache.flink.api.common.functions.ReduceFunction;
-import org.apache.flink.streaming.api.datastream.DataStreamSource;
-import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
-import org.apache.flink.streaming.api.windowing.evictors.Evictor;
-import org.apache.flink.streaming.api.windowing.time.Time;
-import org.apache.flink.streaming.api.windowing.triggers.Trigger;
-import org.apache.flink.streaming.api.windowing.triggers.TriggerResult;
-import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
-import org.apache.flink.streaming.runtime.operators.windowing.TimestampedValue;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 
-import java.time.Duration;
 //nc -lk 9999
 public class catalogtest {
-    public static void main(String[] args) throws Exception {
-        System.out.println("test1");
 
+
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        StreamTableEnvironment tenv = StreamTableEnvironment.create(env);
     }
+
+
 }
