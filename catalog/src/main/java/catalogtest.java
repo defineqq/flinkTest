bd3015dc4dd81d6fba0c61841ebbf8292eb78af3
defineqq <914385955@qq.com>
2023-04-18 11:40:17 +0800

    1

--- catalog/src/main/java/catalogtest.java
+++ catalog/src/main/java/catalogtest.java
@@ -1,13 +1,32 @@
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.DataTypes;
+import org.apache.flink.table.api.Schema;
+import org.apache.flink.table.api.TableDescriptor;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 
-//nc -lk 9999
+///这里用的是flink 1.14.5版本的flink
 public class catalogtest {
 
 
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         StreamTableEnvironment tenv = StreamTableEnvironment.create(env);
+        tenv.createTable("table_a",
+                TableDescriptor.forConnector("filesystem").schema(Schema.newBuilder()
+                    .column("id", DataTypes.INT())
+                    .column("name", DataTypes.STRING())
+                    .column("age", DataTypes.INT())
+                    .column("gender", DataTypes.STRING())
+                .build())
+                .format("csv")
+                .option("path","/Users/congpeng/Desktop/flinkTest/catalog/src/main/resources/")
+                .option("csv.ignore-parse-errors","true")
+                .option("csv.allow-comments","true")
+                .build()
+                );
+
+
+
     }
 
 
