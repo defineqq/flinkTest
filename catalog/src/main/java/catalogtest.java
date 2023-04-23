a31c8443df3fa06497fc6f0722f11159b907f36a
defineqq <914385955@qq.com>
2023-04-23 15:56:58 +0800

    catalog

--- catalog/src/main/java/catalogtest.java
+++ catalog/src/main/java/catalogtest.java
@@ -3,29 +3,32 @@ import org.apache.flink.table.api.DataTypes;
 import org.apache.flink.table.api.Schema;
 import org.apache.flink.table.api.TableDescriptor;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+import org.apache.flink.table.catalog.hive.HiveCatalog;
 
 ///这里用的是flink 1.14.5版本的flink
 public class catalogtest {
 
 
     public static void main(String[] args) throws Exception {
+        System.out.println("啥啥啥");
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         StreamTableEnvironment tenv = StreamTableEnvironment.create(env);
-        tenv.createTable("table_a",
-                TableDescriptor.forConnector("filesystem").schema(Schema.newBuilder()
-                    .column("id", DataTypes.INT())
-                    .column("name", DataTypes.STRING())
-                    .column("age", DataTypes.INT())
-                    .column("gender", DataTypes.STRING())
-                .build())
-                .format("csv")
-                .option("path","/Users/congpeng/Desktop/flinkTest/catalog/src/main/resources/")
-                .option("csv.ignore-parse-errors","true")
-                .option("csv.allow-comments","true")
-                .build()
-                );
+        HiveCatalog hivecatalog = new HiveCatalog("hive","default","conf/hiveconf/hive-site.xml");
+        tenv.registerCatalog("mycatalog",hivecatalog);
 
+        tenv.executeSql("create table mycatalog.table1");
+        tenv.executeSql("select * from  mycatalog.default.table1");//没有指定数据库，默认存放到了hive的default 中
 
+        tenv.executeSql("create view mycatalog.default.view1");
+        tenv.executeSql("select * from  mycatalog.default.view1");
+
+        tenv.executeSql("use catalog mycatalog");
+        tenv.executeSql("show catalogs").print();
+        tenv.executeSql("use catalog default");
+        tenv.executeSql("show catalogs").print();
+
+        tenv.executeSql("create temporary table mycatalog.table1");
+        tenv.listCatalogs()''
 
     }
 
